6c7964bd82753453ffba5c08a16fc0362c4e936c
wangzhizhou <824219521@qq.com>
2020-02-28 17:33:59 +0800

    modified publish script

--- publish_site.sh
+++ publish_site.sh
@@ -19,7 +19,7 @@ git worktree prune
 rm -rf .git/worktrees/${SITE_PUB_DIR}
 
 echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages${SITE_PUB_DIR} origin/gh-pages
+git worktree add -B gh-pages ${SITE_PUB_DIR} origin/gh-pages
 
 echo "Removing existing files"
 rm -rf ${SITE_PUB_DIR}/*
