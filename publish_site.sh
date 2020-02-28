7532d825f2db803e0622d0050a671c3b400dfeee
wangzhizhou <824219521@qq.com>
2020-02-29 00:39:40 +0800

    bugfix

--- publish_site.sh
+++ publish_site.sh
@@ -33,6 +33,6 @@ cd ${SITE_PUB_DIR} && git add --all && git commit -m "Publishing to gh-pages (pu
 
 echo "Pushing to github"
 cd - 
-git push --all
+git push -u --all
 open "http://www.jokerhub.cn"
 
