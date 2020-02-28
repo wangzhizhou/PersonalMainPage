41af6b16421ff1fc99af27706efc42b10c1cff7f
wangzhizhou <824219521@qq.com>
2020-02-29 00:11:50 +0800

    publish

--- publish_site.sh
+++ publish_site.sh
@@ -14,19 +14,11 @@ SITE_THEME_DIR="${SITE_ROOT}/themes"
 
 echo "Deleting old publication"
 rm -rf ${SITE_PUB_DIR}
-<<<<<<< HEAD
-=======
-mkdir ${SITE_PUB_DIR}
->>>>>>> 3f0eb83e52594b3b8f085f2dccde63a904d0e69f
 git worktree prune
 rm -rf .git/worktrees/${SITE_PUB_DIR}
 
 echo "Checking out gh-pages branch into public"
-<<<<<<< HEAD
 git worktree add ${SITE_PUB_DIR} -B gh-pages origin/gh-pages
-=======
-git worktree add -B gh-pages ${SITE_PUB_DIR} origin/gh-pages
->>>>>>> 3f0eb83e52594b3b8f085f2dccde63a904d0e69f
 
 echo "Removing existing files"
 rm -rf ${SITE_PUB_DIR}/*
@@ -37,11 +29,7 @@ echo "Generating site"
 hugo -s "${SITE_ROOT}" -e production
 
 echo "Updating gh-pages branch"
-<<<<<<< HEAD
 cd ${SITE_PUB_DIR} && echo "$base_url" > ${SITE_PUB_DIR}/CNAME && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
-=======
-cd ${SITE_PUB_DIR} && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
->>>>>>> 3f0eb83e52594b3b8f085f2dccde63a904d0e69f
 
 echo "Pushing to github"
 cd - 
